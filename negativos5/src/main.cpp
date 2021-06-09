a31da4da04a39774603d04036ce124c121ffe884
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-09 16:53:51 -0300

    Negativo5
    Start da questão Negativo5 (talvez já completa)

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,19 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int c = 0;
+    int n = 0;
+    while (c < 5)
+    {
+        int x;
+        std::cin >> std::ws>> x;
+        if(x < 0) {
+            n = n + 1;
+        }
+        c = c + 1;
+    }
 
+    std :: cout << n << '\n';
+    
     return 0;
 }

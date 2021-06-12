a3272c2e7c920c18e852669d8612eb1980252302
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-12 14:49:57 -0300

    intervalos concluido

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -15,7 +15,36 @@ using std::setprecision;
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
+    int x;
+    double inUm = 0;
+    double inDois = 0;
+    double inTres = 0;
+    double inQuatro = 0;
+    double noIn = 0;
+    double total = 0;
+
+    while( cin >> std::ws >> x) {
+        if(x >= 100) {
+            noIn = noIn + 1;
+        } else if (x >= 75) {
+            inQuatro = inQuatro + 1;
+        } else if(x >= 50) {
+            inTres = inTres + 1;
+        } else if(x >= 25) {
+            inDois = inDois + 1;
+        } else if(x >= 0) {
+            inUm = inUm + 1;
+        } else {
+            noIn = noIn + 1;
+        }
+        total = total + 1;
+    }
+    std::cout << std::setprecision(4); 
+    std::cout << ((inUm*100) / total) << '\n';
+    std::cout << ((inDois*100) / total) << '\n';
+    std::cout << ((inTres*100) / total) << '\n';
+    std::cout << ((inQuatro*100) / total) << '\n';
+    std::cout << ((noIn*100) / total) << '\n';
 
     return 0;
 }
